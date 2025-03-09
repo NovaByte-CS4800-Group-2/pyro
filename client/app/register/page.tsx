67ed8d7fdda735c2b69e4fa5f32521ff6da99fcc
turtlelover56 <anadavis@gmail.com>
2025-03-09 13:23:54 -0700

    Edit to Header Variability for Efficiency

--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/app/ui/header";
 export default function Register() {
   return (
 	<>
-	  <Header type={1}/>
+	  <Header />
 	  <div className="ml-30 mr-30 border-black border-2 text-center flex flex-col">
 		<h3>Registration Page</h3>
 	  </div>
