3600947f4b9260b8aa2eaced8e9ae821efac0287
turtlelover56 <anadavis@gmail.com>
2025-03-09 13:26:08 -0700

    Moved Header to Layout

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,9 +1,11 @@
 import "./globals.css";
+import Header from "./ui/header";
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
       <body>
+      <Header />
         {children}
       </body>
     </html>
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,7 +3,6 @@ import Header from "@/app/ui/header";
 export default function Home() {
   return (
     <>
-      <Header />
       <div className="ml-30 mr-30 border-black border-2 text-center flex flex-col">
         <h3>Landing Page</h3>
       </div>
