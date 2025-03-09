bf7f837f728cdb8c3a79249919b2cf0d73ccc73a
turtlelover56 <anadavis@gmail.com>
2025-03-08 17:30:13 -0800

    Added links for landing, registration, and login pages

---
+++ client/app/log-in/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/app/ui/header";
+
+export default function Login() {
+  return (
+	<>
+	  <Header />
+	  <div className="ml-30 mr-30 border-black border-2 text-center flex flex-col">
+		<h3>Login Page</h3>
+	  </div>
+	</>
+  );
+}
