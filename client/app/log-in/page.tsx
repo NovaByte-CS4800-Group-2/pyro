c5b42efac9959bdb425dd71b1319142976bcddb5
turtlelover56 <anadavis@gmail.com>
2025-03-09 20:11:39 -0700

    Added nonfunctioning Functions for Posting

--- client/app/log-in/page.tsx
+++ client/app/log-in/page.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import {loginUser} from "@/app/server_functions/functions"
+import Form from "next/form";
 
 export default function Login() {
   return (
@@ -7,13 +9,13 @@ export default function Login() {
 		<h1 className="text-3xl font-display pb-2 font-bold">Log In</h1>
 		<h2 className="text-l font-display">Don't have an account? <Link href="/register" className="font-semibold hover:underline">Sign Up</Link></h2>
 	  	{/* TODO: Add URL to submit to (action) */}
-		<form method="post" className="flex flex-col m-auto mt-8 font-normal">
+		<Form action={loginUser} className="flex flex-col m-auto mt-8 font-normal">
 		<label htmlFor="username" className="self-start">Username</label>
 				<input id="username" type="text" className="border-(liver) border-2 mb-8 p-2"></input>
 				<label htmlFor="password" className="self-start">Password</label>
 				<input id="password" type="password" className="border-(liver) border-2 mb-8 p-2"></input>
 			<Link href="/"><input className="button m-auto text-(--liver)" type="submit" value="Log In"></input></Link>
-		</form>
+		</Form>
 	  </div>
 	</>
   );
---
+++ client/app/server_functions/functions.jsx
@@ -0,0 +1,9 @@
+"use server"
+
+export async function registerUser() {
+
+}
+
+export async function loginUser() {
+	
+}
