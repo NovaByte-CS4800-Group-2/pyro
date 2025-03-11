7b635cf56d1d51d1f3c35f0397befa1a471ca006
jessp60 <jmpinto@cpp.edu>
2025-03-10 21:26:57 -0700

    sign in verification

--- server/loginDatabase.js
+++ server/loginDatabase.js
@@ -4,27 +4,63 @@ import {hash} from './sha256.js'
 import dotenv from 'dotenv' 
 dotenv.config()
 
-const hashString = hash("hello");
-
 const pool = mysql.createPool({
     host: "localhost", 
     user: "root", 
-    password: "Server123",
+    password: "",
     database: "notes_app"
 }).promise()
 
-export async function comparePasswords(string){
+/* Checks whether a username and password combination matches the database values. */
+async function checkCredentials(inputPass, userName){
 
-    const hashedString = hash(string);
-    console.log(hashedString);
-    const [rows] = await pool.query("SELECT * from passTest where userName = ?", [hashedString]);
-    console.log(rows)
+    const hashedString = hash(inputPass);
+    if (await getPassword(userName) == (hashedString)) {
+        return true; 
+    }
+    else {
+        return false;
+    }
 }
 
-comparePasswords("pintoBean")
+/* Checks if an input username exists in the database. */
+async function checkUsername (inputUsername){
+    const [rows] = await pool.query("SELECT * from passTest where userName = ?", [inputUsername])
+    for (let i = 0; i < rows.length; i++){
+        if (rows[i].userName == inputUsername){
+            return true
+        }
+    }
+    return false
+}
+/* Finds a user's username from the database given their password. */
+async function getUsername(password){
+    try{
+        const hashedPassword = hash(password)
+        const [rows] = await pool.query("SELECT * from passTest where password = ?", [hashedPassword])
+        return rows[0].userName
+        // !!! in order to actually get the results of this without "promise pending", use await before CALLING the function 
+        // ex: console.log(await getUsername("password"))
+    } catch (error) {
+        return null // if the password is not in the table 
+    }   
+}
+
+/* Finds a user's password from the database given their username. */
+async function getPassword(username){
+    try {
+        const [rows] = await pool.query("SELECT * from passTest where userName = ? LIMIT 1", [username])
+        return rows[0].password
+    } catch (error){
+        return null // if user does not exist, there won't be a password to find 
+    }
+    
+}
 
 
-// compare passwords
-// compare usernames 
-// get username based on password 
-// get password based on username
+console.log("The username for this password is:", await getUsername("password"))
+console.log("Checking if the username 'beepBepp' exists..,", await checkUsername("beepBepp"))
+console.log("Getting the password for pintoBean:", await getPassword("pintoBean"))
+console.log("Checking if pintoBean's password is 'password'", await checkCredentials("password", "pintoBean"))
+
+await pool.end() // close the connection once information has been gathered
