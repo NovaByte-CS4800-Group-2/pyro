d06b3741cbba702d50772af90a8a3db0f3340232
areencpp <boyadjian@cpp.edu>
2025-03-10 18:17:15 -0700

    adding img carousel

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/app/ui/header";
 
+'use client'
 export default function Home() {
   return (
     <>
@@ -9,3 +10,28 @@ export default function Home() {
     </>
   );
 }
+
+import React from 'react'
+import useEmblaCarousel from 'embla-carousel-react'
+import Autoplay from 'embla-carousel-autoplay'
+
+export function EmblaCarousel() {
+  const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay({delay:2000})])
+
+  return (
+    <div className="embla">
+      <div className="embla viewport max-w-lg mt-12 mx-auto h-56 border" ref={emblaRef}>
+        <div className="embla__container h-full">
+          <div className="embla__slide flex items-center justify-center">Slide 1</div>
+          <div className="embla__slide flex items-center justify-center">Slide 2</div>
+          <div className="embla__slide flex items-center justify-center">Slide 3</div>
+        </div>
+      </div>
+      
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
