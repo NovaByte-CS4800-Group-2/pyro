179b318e05f42d9fd2882dbf41175047c9283221
turtlelover56 <anadavis@gmail.com>
2025-03-11 14:30:06 -0700

    Created Register User Function that (hopefully) Works

--- client/app/log-in/page.tsx
+++ client/app/log-in/page.tsx
@@ -9,12 +9,11 @@ export default function Login() {
 	  <div className="mt-10 ml-30 mr-30 text-center flex flex-col">
 		<h1 className="text-3xl font-display pb-2 font-bold">Log In</h1>
 		<h2 className="text-l font-display">Don't have an account? <Link href="/register" className="font-semibold hover:underline">Sign Up</Link></h2>
-	  	{/* TODO: Add URL to submit to (action) */}
 		<Form action={loginUser} className="flex flex-col m-auto mt-8 font-normal">
 		<label htmlFor="username" className="self-start">Username</label>
-				<input id="username" type="text" className="border-(liver) border-2 mb-8 p-2"></input>
+				<input id="username" name="username" type="text" className="border-(liver) border-2 mb-8 p-2"></input>
 				<label htmlFor="password" className="self-start">Password</label>
-				<input id="password" type="password" className="border-(liver) border-2 mb-8 p-2"></input>
+				<input id="password" name="password" type="password" className="border-(liver) border-2 mb-8 p-2"></input>
 			{/* TODO: Switch to use Button component. */}
 			<Link href="/"><input className="button m-auto text-(--liver) hover:bg-(--moss-green)" type="submit" value="Log In"></input></Link>
 		</Form>
--- client/app/server_functions/functions.jsx
+++ client/app/server_functions/functions.jsx
@@ -1,7 +1,17 @@
 "use server"
+import {createProfile} from "../../../server/profile";
 
-export async function registerUser() {
 
+export async function registerUser(formData) {
+	const name = formData.get("name");
+	const email = formData.get("email");
+	const username = formData.get("username");
+	const zipcode = formData.get("zipcode");
+	const password = formData.get("password");
+	const confirmPassword = formData.get("confirm-password");
+	if (name & email & username & zipcode & password & confirmPassword) {
+		createProfile(0, username, name, email, zipcode, password, false);
+	}
 }
 
 export async function loginUser() {
