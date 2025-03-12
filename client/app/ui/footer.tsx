2e93062807e9371befafbaf3fbcf46629da3376a
areencpp <boyadjian@cpp.edu>
2025-03-11 19:35:16 -0700

    edited button

--- client/app/ui/footer.tsx
+++ client/app/ui/footer.tsx
@@ -7,9 +7,9 @@ export default function Footer() {
 	return (
 		<div className="flex bg-(--liver) border-t-2 border-(--liver) w-full h-18 font-display mt-auto">
 			<div className="w-1/3 self-end pb-1 pl-1">
-  				<p style={{ color: "var(--sage)" }}>&copy; 2025 Nova Byte</p> 
+  				<p style={{ color: "var(--seashell)" }}>&copy; 2025 Nova Byte</p> 
   			</div>
-			<Link target="_blank" href="https://novabyte-cs4800-group-2.github.io/NovaByte-website/" className="flex justify-center items-center w-1/3 bg-(--sage)">
+			<Link target="_blank" href="https://novabyte-cs4800-group-2.github.io/NovaByte-website/" className="flex justify-center items-center w-1/3 bg-(--liver)">
 				<Image src="/logo-transparent-svg.svg" alt="The Nova Byte Logo" width={300} height={81}></Image>
 			</Link>
 			<div className="w-1/3 justify-end flex m-auto pr-5"><Button label="Contact Us"></Button></div>
--- client/app/ui/header.tsx
+++ client/app/ui/header.tsx
@@ -19,8 +19,9 @@ export default function Header() {
 	return (
 		<div className="flex bg-(--liver) border-b-2 border-(--liver) w-full h-18 font-display">
 		<div className="w-1/3" />
-		<Link href="/" className="flex justify-center items-center w-1/3 bg-(--sage)">
+		<Link href="/" className="flex justify-center items-center w-1/3 bg-(--liver)">
 			<FireIcon className="text-(--cocoa-brown) pt-2 w-9.5"/>
-			<h1 className="text-4xl font-bold">Pyro</h1>
+			<h1 className="text-4xl font-bold" style={{ color: "var(--seashell)" }}>Pyro</h1>
+
 		</Link>{html}</div>);
 }
