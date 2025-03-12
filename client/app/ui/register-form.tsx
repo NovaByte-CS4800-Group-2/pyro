6a22c7e6a7b5614dd9d2e3f9d24314669ce30f70
turtlelover56 <anadavis@gmail.com>
2025-03-11 21:54:22 -0700

    Edits to Registration, does not validate fully

--- client/app/ui/register-form.tsx
+++ client/app/ui/register-form.tsx
@@ -1,26 +1,56 @@
 "use client"
 import Form from "next/form";
 import { registerUser } from "@/app/server_functions/functions";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+	name: string
+	email: string
+	username: string
+	zipcode: string
+	password: string
+	confirmPassword: string
+}
 
 export default function RegisterForm() {
-	return <Form action={registerUser} 
-			className="flex flex-col w-full max-w-80 m-auto mt-8 font-normal">
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm<Inputs>();
+	const onSubmit: SubmitHandler<Inputs> = registerUser;
+
+	watch("name");
+	watch("email");
+	watch("username");
+	watch("zipcode");
+	watch("password");
+	watch("confirmPassword");
+
+	return <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-full max-w-80 m-auto mt-8 font-normal">
 			<label htmlFor="name" className="self-start">Name</label>
-			<input id="name" name="name" type="text" className="border-(liver) border-2 mb-6 p-2"></input>
+			<input {...register("name", { required: true })} aria-invalid={errors.name ? "true" : "false"} id="name" type="text" className="border-(liver) border-2 mb-6 p-2"></input>
+			{errors.name && <span>This field is required</span>}
 			<label htmlFor="email" className="self-start">Email</label>
-			<input id="email" name="email" type="email" className="border-(liver) border-2 mb-6 p-2"></input>
+			<input {...register("email", { required: true })} id="email" type="email" className="border-(liver) border-2 mb-6 p-2"></input>
+			{errors.email && <span role="alert">This field is required</span>}
 			<label htmlFor="username" className="self-start">Username</label>
-			<input id="username" name="username" type="text" className="border-(liver) border-2 mb-6 p-2"></input>
+			<input {...register("username", { required: true })} id="username" type="text" className="border-(liver) border-2 mb-6 p-2"></input>
+			{errors.username && <span>This field is required</span>}
 			<div className="flex justify-between">
 				<label htmlFor="zipcode" className="self-start">Zip Code</label>
 				{/* TODO: Add pop-up dialog when link is clicked. */}
 				<a className="text-sm mt-0.75 underline text-blue-500" href="/">Why we need it</a>
 			</div>
-			<input id="zipcode" name="zipcode" type="text" inputMode="numeric" pattern="[0-9]{5}" className="border-(liver) border-2 mb-6 p-2"></input>
+			<input {...register("zipcode", { required: true, pattern: /[0-9]{5}/i })} id="zipcode" type="text" inputMode="numeric" pattern="[0-9]{5}" className="border-(liver) border-2 mb-6 p-2"></input>
+			{errors.zipcode && <span>This field is required</span>}
 			<label htmlFor="password" className="self-start">Password</label>
-			<input id="password" name="password" type="password" className="border-(liver) border-2 mb-6 p-2"></input>
+			<input {...register("password", { required: true })} id="password" type="password" className="border-(liver) border-2 mb-6 p-2"></input>
+			{errors.password && <span>This field is required</span>}
 			<label htmlFor="confirm-password" className="self-start">Confirm Password</label>
-			<input id="confirm-password" name="confirm-password" type="password" className="border-(liver) border-2 mb-8 p-2"></input>
+			<input {...register("confirmPassword", { required: true })} id="confirm-password" type="password" className="border-(liver) border-2 mb-8 p-2"></input>
+			{errors.confirmPassword && <span>This field is required</span>}
 			<button className="button m-auto text-(--liver)" type="submit">Sign Up</button>
-		</Form>;
+		</form>;
 }
