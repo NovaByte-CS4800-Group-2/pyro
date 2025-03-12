1b268558c84bf581fe41b2670463b9f2f70f61d3
areencpp <boyadjian@cpp.edu>
2025-03-11 19:35:16 -0700

    edited button

--- client/app/ui/button.tsx
+++ client/app/ui/button.tsx
@@ -1,6 +1,10 @@
-import Link from "next/link"
-import "@/app/globals.css"
+import Link from "next/link";
+import "@/app/globals.css";
 
-export default function Button({ label = "Button", link = "/", color = "(--liver)", bg = "(--sage)" , hover = "(--moss-green)" }) {
-	return <Link href={link} className={"font-display rounded-lg p-0.5 pl-2 pr-2 w-fit h-fit hover:bg-" + hover + " bg-" + bg + " text-" + color}>{label}</Link>;
+export default function Button({ label = "Button", link = "/" }) {
+  return (
+    <Link href={link} className="button">
+      {label}
+    </Link>
+  );
 }
