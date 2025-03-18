1bc27222162b0999fab50a5e0a1bb87f3df3caf5
areencpp <boyadjian@cpp.edu>
2025-03-17 19:49:34 -0700

    landing page edit

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -8,6 +8,19 @@ export default function Landing() {
       <div>
         <EmblaCarousel/>
       </div>
+
+      <div style={{
+        width: "100%",
+        maxWidth: "1024px", 
+        margin: "0 auto",
+        border: "2px solid var(--dark-brown)",
+        padding: "16px",
+        textAlign: "center",
+        borderRadius: "8px",
+        marginTop: "24px"
+      }}>
+        Our mission: 
+      </div>
     </>
   );
 }
