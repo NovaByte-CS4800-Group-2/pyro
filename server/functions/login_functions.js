e26f838847966d12fb8fc204979983299284a747
jessp60 <jmpinto@cpp.edu>
2025-03-17 20:31:30 -0700

    fix log in

--- server/functions/login_functions.js
+++ server/functions/login_functions.js
@@ -2,7 +2,7 @@ import {hash} from './sha256.js'
 import pool from './pool.js'
 
 /* Checks whether a username and password combination matches the database values. */
-export async function checkCredentials(inputPass, userName){
+export async function checkCredentialsUsername(inputPass, userName){
 
     const hashedString = hash(inputPass);
     if (await getPassword(userName) === hashedString) {
@@ -13,8 +13,29 @@ export async function checkCredentials(inputPass, userName){
     }
 }
 
+export async function checkCredentials(inputPass, email){
+
+    const hashedString = hash(inputPass);
+    if (await getPassword(email) === hashedString) {
+        c
+        return true; 
+    }
+    else {
+        return false;
+    }
+}
+
+export async function getPassword(email){
+    try {
+        const [rows] = await pool.query("SELECT * from users where email = ? LIMIT 1", [email])
+        return rows[0].password
+    } catch (error){
+        return null // if user does not exist, there won't be a password to find 
+    } 
+}
+
 /* Finds a user's password from the database given their username. */
-export async function getPassword(username){
+export async function getPasswordUsername(username){
     try {
         const [rows] = await pool.query("SELECT * from users where userName = ? LIMIT 1", [username])
         return rows[0].password
