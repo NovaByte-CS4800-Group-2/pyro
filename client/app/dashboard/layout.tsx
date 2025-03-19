195f8624f9ac910bc73e8237e3436b71d766bb9d
turtlelover56 <anadavis@gmail.com>
2025-03-19 09:23:03 -0700

    Began work on dashboard

--- client/app/dashboard/layout.tsx
+++ client/app/dashboard/layout.tsx
@@ -3,6 +3,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/app/firebase/config";
 import { useRouter } from "next/navigation";
 import "@/app/globals.css";
+import Navbar from "../ui/navbar";
 
 export default function RootLayout({
   children,
@@ -19,5 +20,9 @@ export default function RootLayout({
   if (!user && !userSession) {
     return router.push("/");
   }
-  return <div>{children}</div>;
+  return (
+    <div className="flex flex-row border-2 min-h-screen">
+      <Navbar></Navbar>
+      {children}
+    </div>);
 }
---
+++ client/app/ui/navbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import "@/app/globals.css";
+
+export default function Navbar() {
+  return (
+    <div className="flex flex-col min-h-screen bg-(--moss-green) lg:text-xl md:text-lg sm:text-md font-bold">
+		<Link href="" className="bg-(--sage) border-(--moss-green) p-5 border-2 text-center border-b-1 hover:bg-(--moss-green)">Forum</Link>
+		<Link href="" className="bg-(--sage) border-(--moss-green) p-5 border-2 text-center border-b-1 hover:bg-(--moss-green)">Fundraisers</Link>
+		<Link href="" className="bg-(--sage) border-(--moss-green) p-5 border-2 text-center border-b-1 hover:bg-(--moss-green)">Resources</Link>
+		<Link href="" className="bg-(--sage) border-(--moss-green) p-5 border-2 text-center hover:bg-(--moss-green)">Matching Requests</Link>
+	</div>
+  );
+}
