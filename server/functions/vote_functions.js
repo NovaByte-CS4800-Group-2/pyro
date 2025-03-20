13817cbc8df3d0388744bdaf6e3ba532c59bc88b
jessp60 <jmpinto@cpp.edu>
2025-03-20 14:05:36 -0700

    fix syntax

--- server/functions/vote_functions.js
+++ server/functions/vote_functions.js
@@ -3,7 +3,7 @@ import pool from "./pool.js";
 class Vote{
     async upvote(content_id, user_id){
         try {
-            await pool.query("INSERT INTO votes (content_id, user_id, value), (?, ?, 1)", [content_id, user_id])
+            await pool.query("INSERT INTO votes (content_id, user_id, value) VLAUES (?, ?, 1)", [content_id, user_id])
         } catch (e) {
             console.log(e)
         }
@@ -11,9 +11,12 @@ class Vote{
 
     async downvote(content_id, user_id){
         try {
-            await pool.query("INSERT INTO votes (content_id, user_id, value), (?, ?, 0)", [content_id, user_id])
+            await pool.query("INSERT INTO votes (content_id, user_id, value) VALUES(?, ?, 0)", [content_id, user_id])
         } catch (e) {
             console.log(e)
         }
     }
 }
+
+const vote = new Vote(); 
+await vote.downvote(1,12);
