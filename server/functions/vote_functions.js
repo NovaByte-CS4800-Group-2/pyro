3bbaf2555de89c7e58a9b5c8f4c20ac7651c111c
jessp60 <jmpinto@cpp.edu>
2025-03-20 14:02:34 -0700

    voting functions

--- server/functions/vote_functions.js
+++ server/functions/vote_functions.js
@@ -1,5 +1,19 @@
 import pool from "./pool.js";
 
 class Vote{
-    async upVote(content_id, user_id)
+    async upvote(content_id, user_id){
+        try {
+            await pool.query("INSERT INTO votes (content_id, user_id, value), (?, ?, 1)", [content_id, user_id])
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    async downvote(content_id, user_id){
+        try {
+            await pool.query("INSERT INTO votes (content_id, user_id, value), (?, ?, 0)", [content_id, user_id])
+        } catch (e) {
+            console.log(e)
+        }
+    }
 }
