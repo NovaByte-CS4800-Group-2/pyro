e9119a01a33d28a5be60c4a144ae36b7d9f32c44
turtlelover56 <anadavis@gmail.com>
2025-03-22 18:35:08 -0700

    Added HeroUIProvider

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,21 +1,27 @@
 import "./globals.css";
 import Header from "./ui/header";
 import Footer from "./ui/footer";
-
+import {HeroUIProvider} from '@heroui/react'
+import {ToastProvider} from "@heroui/toast";
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
-      <body className="flex flex-col min-h-screen">
-      <header className="fixed top-0 left-0 w-full shadow-md z-50">
-          <Header />
-        </header>
-        <main className="flex flex-col flex-grow mt-18 mb-18 overflow-auto">
-          {children}
-        </main>
-        <footer className="fixed bottom-0 left-0 w-full shadow-md z-50">
-          <Footer />
-        </footer>
+      <body>
+      <HeroUIProvider>
+        <ToastProvider />
+        <div className="flex flex-col min-h-screen">
+          <header className="fixed top-0 left-0 w-full shadow-md z-50">
+              <Header />
+            </header>
+            <main className="flex flex-col flex-grow mt-18 mb-18 overflow-auto">
+              {children}
+            </main>
+            <footer className="fixed bottom-0 left-0 w-full shadow-md z-50">
+              <Footer />
+            </footer>
+          </div>
+        </HeroUIProvider>
       </body>
     </html>
   );
