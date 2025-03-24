8a11fe78771b78aa6dc2ea2123151e5ce0cca0c1
Hadya Rohin <hadya.rohin@gmail.com>
2025-03-23 23:17:42 -0700

    added createpost page and buttons

--- client/app/ui/button.tsx
+++ client/app/ui/button.tsx
@@ -1,10 +1,31 @@
 import Link from "next/link";
 import "@/app/globals.css";
+import React from "react";
 
-export default function Button({ label = "Button", link = "/" }) {
-  return (
-    <Link href={link} className="button">
-      {label}
-    </Link>
-  );
+interface ButtonProps {
+  label: string;
+  link?: string;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  type?: "button" | "submit" | "reset";
+}
+
+export default function Button({
+  label = "Button",
+  link = "/",
+  onClick,
+  type = "button",
+}: ButtonProps) {
+  if (link) {
+    return (
+      <Link href={link} className="button">
+        {label}
+      </Link>
+    );
+  } else {
+    return (
+      <button type={type} onClick={onClick} className="button">
+        {label}
+      </button>
+    );
+  }
 }
