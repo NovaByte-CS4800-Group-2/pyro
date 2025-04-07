be244138bfaa67391444ef96a20a039bcb70b83d
jessp60 <jmpinto@cpp.edu>
2025-04-07 13:10:43 -0700

    update fundraiser page

--- client/app/dashboard/createpost/page.tsx
+++ client/app/dashboard/createpost/page.tsx
@@ -21,6 +21,7 @@ export default function CreatePost() {
     user_id: 0,
     username: "",
     city: "",
+    business_account: 0,
   }); // State for user data
   const [subforums, setSubforums] = useState<any[]>([]); // State to store subforums
 
@@ -108,7 +109,7 @@ export default function CreatePost() {
   };
 
   // Handle form submission
-  const handleSubmit = async (event: React.FormEvent) => {
+  const handlePersonalSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
 
     console.log("Submitting post...");
@@ -153,6 +154,56 @@ export default function CreatePost() {
       console.log("Post submitted successfully:", postData);
 
       router.push("/dashboard");
+    } catch (error: any) {
+      setErrorMessage(error.message || "An unexpected error occurred.");
+      console.error("Error submitting post:", error);
+    }
+  };
+
+  const handleBusinessSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    console.log("Submitting post...");
+    console.log("Post content:", postContent.body);
+    console.log("User data:", userData);
+
+    if (!postContent.body.trim()) {
+      setErrorMessage("Post cannot be empty.");
+    }
+
+    if (!city) {
+      setErrorMessage("Please select a subforum.");
+    }
+
+    if (!userData || !userData.username) {
+      setErrorMessage("User data is incomplete. Please try again.");
+    }
+
+    try {
+      const requestBody = {
+        city, // Use the selected subforum name as city
+        username: userData.username,
+        body: postContent.body,
+      };
+      console.log("Request body:", requestBody);
+
+      const postResponse = await fetch("http://localhost:8080/post", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(requestBody),
+      });
+
+      console.log("Post response:", postResponse);
+
+      if (!postResponse.ok) {
+        const errorData = await postResponse.json();
+        throw new Error(errorData.error || "Failed to submit post.");
+      }
+
+      const postData = await postResponse.json();
+      console.log("Post submitted successfully:", postData);
+
+      router.push("/dashboard/fundraiser");
     } catch (error: any) {
       setErrorMessage(error.message || "An unexpected error occurred.");
       console.error("Error submitting post:", error);
@@ -207,14 +258,23 @@ export default function CreatePost() {
                 <option value="" disabled>
                   -- Select a Subforum --
                 </option>
-                {subforums.map((subforum) => (
-                  <option
-                    key={subforum.subforum_id}
-                    value={subforum.name} // Use subforum name as the value
-                  >
-                    {subforum.name}
-                  </option>
-                ))}
+                {subforums.map((subforum) => {                  
+                  if (!userData.business_account && subforum.subforum_id != 0) {
+                    return <option
+                      key={subforum.subforum_id}
+                      value={subforum.name} // Use subforum name as the value
+                    >
+                      {subforum.name}
+                    </option>
+                  } else if (userData.business_account && subforum.subforum_id == 0) {
+                    return <option
+                      key={subforum.subforum_id}
+                      value={subforum.name} // Use subforum name as the value
+                    >
+                      {subforum.name}
+                    </option>
+                  }
+                  })}
               </select>
             </div>
 
@@ -260,7 +320,7 @@ export default function CreatePost() {
               <Button
                 type="submit"
                 label="Post"
-                onClick={handleSubmit}
+                onClick={userData.business_account ? handleBusinessSubmit: handlePersonalSubmit}
                 disabled={!postContent.body.trim()}
               />
              {/* <div className="flex justify-start">
