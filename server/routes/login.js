bfa6f70b78088de0943d54bca15809de197ff1c1
mamikonyan, natalie <nataliemamikonyan@gmail.com>
2025-04-06 19:25:10 -0700

    removed exress routing

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,5 +1,4 @@
 import { Router } from "express";  // create an instance of an express router
-import cors from "cors";
 import Login from "../functions/login_functions.js";
 
 const router = Router();  // groups together requests
@@ -18,17 +17,7 @@ router.post('/login', async (req, res) => {  // does authentication
   const isValid = await login.checkCredentials();  // checking if username and password match
   if(!isValid) return res.status(401).json({ error: "Invalid credentials" });  // no existing profile
 
-  req.session.user = user;  // store user in session if found
   return res.status(200).json(user);
 })
-   
-router.get('/login/status', async (req, res) => {  // gets authentication status
-
-  req.sessionStore.get(req.sessionID, (err, session) => {  // to see how everything is stored in memory
-    console.log(session);
-  })
-  return req.session.user ? res.status(200).send(req.session.user) 
-                          : res.status(401).send({ error: "Invalid credentials" })
-})
-
+  
 export default router;
