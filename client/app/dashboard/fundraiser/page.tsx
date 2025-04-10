8d88a5cb03fcc37f28e39228391bc4e34e00b42c
mamikonyan, natalie <nataliemamikonyan@gmail.com>
2025-04-09 19:20:42 -0700

    changed background for fundraiser

--- client/app/dashboard/fundraiser/page.tsx
+++ client/app/dashboard/fundraiser/page.tsx
@@ -6,9 +6,8 @@ import Forum from "../../ui/forum";
 export default function Dashboard() {
 	return (
 		<>
-			<div className="bg-neutral-200 flex-grow min-h-full pl-2 pr-2">
-				<div className="gap-y-5 flex flex-col items-stretch pt-10 pb-8 m-auto w-auto self-center max-w-[700px]">
-					<p className="font-bold text-center">Welcome to the fundraiser forum!</p>
+			<div className="bg-[--greige-mist] flex-grow min-h-full pl-2 pr-2">
+				<div className="gap-y-5 flex flex-col items-stretch pt-10 pb-8 m-auto w-auto self-center max-w-[1000px]">
 					<Forum subforumID="0" />
 				</div>
 			</div>
