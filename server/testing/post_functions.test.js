5278c179a41965b61416758e0c0e3e5e44df7e19
jessp60 <jmpinto@cpp.edu>
2025-04-15 12:00:26 -0700

    completed post_functions tests

--- server/testing/post_functions.test.js
+++ server/testing/post_functions.test.js
@@ -10,7 +10,7 @@ afterAll (async () => {
 })
 
 
-// Testing the editUsername function 
+// Testing the createPost function 
 test ("Create new post.", async () => {
   //const result = await Post.createPost("General", "Ananas", "Unit test post!", 0)
   
@@ -34,7 +34,7 @@ test ("Delete post", async() => {
 
 // Testing getSuborumPosts to get all the posts for a particular subforum
 test ("Get subforum posts", async () => {
-    const result = await Post.getSubforumPosts(2)
+    const result = await Post.getSubforumPosts(3)
 
     // Since the date from the database returns as an object, convert into string before comparing
     for (let i = 0; i < result.length; i++){
@@ -44,7 +44,6 @@ test ("Get subforum posts", async () => {
             result[i].last_edit_date = result[i].last_edit_date.toISOString()
         }
     }
-    console.log(result[0].post_date)
 
     const expected = [
         {
@@ -75,3 +74,89 @@ test ("Get subforum posts", async () => {
     
     expect(result).toEqual(expected) 
 })
+
+// Testing the getUserPosts function that takes in a user id and returns all their posts 
+test ("Get all of a user's posts", async () => {
+    const result = await Post.getUserPosts("k7KWqmBB6UhpYDHiMyMArmGUoqj2")
+
+    // Since the date from the database returns as an object, convert into string before comparing
+    for (let i = 0; i < result.length; i++){
+        result[i].post_date = result[i].post_date.toISOString()
+        // Last edit date can be null, do not convert into string then
+        if (result[i].last_edit_date){
+            result[i].last_edit_date = result[i].last_edit_date.toISOString()
+        }
+    }
+
+    const expected = [
+        {
+          "content_id": 1,
+          "subforum_id": 1,
+          "user_id": "k7KWqmBB6UhpYDHiMyMArmGUoqj2",
+          "post_date": "2025-03-19T07:00:00.000Z",
+          "last_edit_date": null,
+          "body": "You know my dog can be such a menace. She's a border collie so she's got a lot of energy, but even for a border collie she's a little bit odd. It takes all five of us just to keep her entertained and not throwing a fit. Also she can scream like a child. Literally. She hasn't done it in a while, but sometimes I worry she'll start screaming in the middle of the night..."
+        },
+        {
+          "content_id": 10,
+          "subforum_id": 1,
+          "user_id": "k7KWqmBB6UhpYDHiMyMArmGUoqj2",
+          "post_date": "2025-04-15T07:00:00.000Z",
+          "last_edit_date": null,
+          "body": "Unit test post!"
+        },
+        {
+          "content_id": 14,
+          "subforum_id": 2,
+          "user_id": "k7KWqmBB6UhpYDHiMyMArmGUoqj2",
+          "post_date": "2025-04-15T07:00:00.000Z",
+          "last_edit_date": null,
+          "body": "This is a post on the pomona forum for the Jest Test lol\n"
+        },
+        {
+          "content_id": 15,
+          "subforum_id": 3,
+          "user_id": "k7KWqmBB6UhpYDHiMyMArmGUoqj2",
+          "post_date": "2025-04-15T07:00:00.000Z",
+          "last_edit_date": null,
+          "body": "i need a post for unit testing"
+        },
+        {
+          "content_id": 16,
+          "subforum_id": 3,
+          "user_id": "k7KWqmBB6UhpYDHiMyMArmGUoqj2",
+          "post_date": "2025-04-15T07:00:00.000Z",
+          "last_edit_date": null,
+          "body": "here's another post i would like to use for unit testing"
+        }
+      ]
+
+      expect(result).toEqual(expected)
+})
+
+// Testing the getUserPost function that takes in a post id and returns the post
+test ("Get one of a user's posts", async () => {
+
+    const result = await Post.getUserPost(2)
+
+    // Convert all dates to string for comaprison
+    result.post_date = result.post_date.toISOString()
+    // Last edit date can be null, do not convert into string then
+    if (result.last_edit_date){
+        result.last_edit_date = result.last_edit_date.toISOString()
+    }
+
+    const expected = 
+        {
+          "content_id": 2,
+          "subforum_id": 1,
+          "user_id": "fgvfZQxY1SghMxpFvXgjauiUdUo1",
+          "post_date": "2025-03-18T07:00:00.000Z",
+          "last_edit_date": "2025-03-18T07:00:00.000Z",
+          "body": "What is this app even for?"
+        }
+      
+    
+    expect(result).toEqual(expected)
+})
+
