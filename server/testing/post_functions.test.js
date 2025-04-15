34bc08eb941df8d1ae46cd2b17f63ef91bcf4759
jessp60 <jmpinto@cpp.edu>
2025-04-15 11:33:13 -0700

    post functions unit testing

--- server/testing/post_functions.test.js
+++ server/testing/post_functions.test.js
@@ -32,21 +32,46 @@ test ("Delete post", async() => {
     expect(true).toEqual(expected)
 })
 
+// Testing getSuborumPosts to get all the posts for a particular subforum
 test ("Get subforum posts", async () => {
     const result = await Post.getSubforumPosts(2)
+
+    // Since the date from the database returns as an object, convert into string before comparing
+    for (let i = 0; i < result.length; i++){
+        result[i].post_date = result[i].post_date.toISOString()
+        // Last edit date can be null, do not convert into string then
+        if (result[i].last_edit_date){
+            result[i].last_edit_date = result[i].last_edit_date.toISOString()
+        }
+    }
     console.log(result[0].post_date)
-    for (let i = 0; i < result.length; i++) {
-        console.log(typeof result[i].date)
-      }
 
-    const expected = [{
-      "content_id": 3,
-      "subforum_id": 2,
-      "user_id": "xByyjxajyxcV0g2p0du1kDsbTIJ3",
-      "post_date": "2025-03-19T07:00:00.000Z",
-      "last_edit_date": "2025-03-20T07:00:00.000Z",
-      "body": "Why is it alway's raining here???"
-    }]
-
-    expect(result).toContainEqual(expected)
+    const expected = [
+        {
+          "content_id": 15,
+          "subforum_id": 3,
+          "user_id": "k7KWqmBB6UhpYDHiMyMArmGUoqj2",
+          "post_date": "2025-04-15T07:00:00.000Z",
+          "last_edit_date": null, // compares null directly without string conversion
+          "body": "i need a post for unit testing"
+        },
+        {
+          "content_id": 16,
+          "subforum_id": 3,
+          "user_id": "k7KWqmBB6UhpYDHiMyMArmGUoqj2",
+          "post_date": "2025-04-15T07:00:00.000Z",
+          "last_edit_date": null, // compares null directly without string conversion
+          "body": "here's another post i would like to use for unit testing"
+        },
+        {
+          "content_id": 18,
+          "subforum_id": 3,
+          "user_id": "xByyjxajyxcV0g2p0du1kDsbTIJ3",
+          "post_date": "2025-04-15T07:00:00.000Z",
+          "last_edit_date": "2025-04-15T07:00:00.000Z",
+          "body": "perhaps a third one too? (i've edited it)"
+        }
+      ]
+    
+    expect(result).toEqual(expected) 
 })
