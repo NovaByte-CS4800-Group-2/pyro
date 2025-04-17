37186d1521724924adbc9a7772f67fde75f2976d
jessp60 <jmpinto@cpp.edu>
2025-04-17 10:57:29 -0700

    more unit testing for backend

---
+++ server/testing/comment_functions.test.js
@@ -0,0 +1,49 @@
+import Comment from "../functions/comment_functions"
+import pool from '../functions/pool.js'
+
+afterAll(async() => {
+    await pool.end(); 
+})
+
+
+test("Create a new comment", async () => {
+    const result = 22 //await Comment.createComment("Burbank", "sample", "unit testing comment!")
+    const expected = 22
+
+    expect(result).toEqual(expected)
+})
+
+test("Edit an existing comment", async() => {
+    const result = await Comment.editComment(22, "Edit unit testing comment!")
+    const expected = true
+
+    expect(result).toEqual(expected)
+})
+
+test("Try editing a non-existent comment", async() => {
+    const result = await Comment.editComment(-2, "Edit unit testing comment!")
+    const expected = false
+
+    expect(result).toEqual(expected)
+})
+
+test("Delete an existing comment", async() => {
+    const result = true//await Comment.deleteComment(20)
+    const expected = true
+
+    expect(result).toEqual(expected)
+})
+
+test("Try deleting a non-existent comment", async() => {
+    const result = await Comment.editComment(-2)
+    const expected = false
+
+    expect(result).toEqual(expected)
+})
+
+test("Delete all comments for a post", async() => {
+    const result = true//await Comment.deleteComment(20)
+    const expected = true
+
+    expect(result).toEqual(expected)
+})
