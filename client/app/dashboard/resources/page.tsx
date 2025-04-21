b3f82da69e9f0a0a1612eb9f8c7000cb33ac8240
jessp60 <jmpinto@cpp.edu>
2025-04-20 18:18:41 -0700

    added all info for resource page

--- client/app/dashboard/resources/page.tsx
+++ client/app/dashboard/resources/page.tsx
@@ -2,13 +2,14 @@
 
 export default function Dashboard() {
 	return (
-		<div className="flex flex-col items-center gap-y-20 mx-6 w-full h-full pt-1">
+		<div className="flex flex-col text-center gap-y-10 mx-6 w-full h-fullpt-1">
 			<h1 className = "text-3xl mt-5">Resources Page</h1>
-			<div className="flex flex-wrap gap-x-20 gap-y-20">
+			<h2 className="text-left text-2xl">Wildfire Preparedness</h2>
+			<div className="flex flex-wrap gap-x-10 gap-y-10"> 
 				{/* Resource Card */}
-				<div className="max-w-sm bg-white rounded-2xl shadow-md">
+				<div className="max-w-xs text-left bg-white rounded-2xl shadow-md">
 					<img 
-						src="/images/resource1.png" 
+						src="/images/resource1a.png" 
 						alt="Evacuation Essentials"
 						className="w-full object-cover rounded-t-2xl"
 					/>
@@ -16,35 +17,173 @@ export default function Dashboard() {
 						<p className="text-sm text-gray-600 uppercase tracking-wide mb-2">Ready for Wildfire</p>
 						<a href="https://readyforwildfire.org/prepare-for-wildfire/emergency-supply-kit/">
 							<h2 className="text-lg font-semibold text-gray-800 mb-2">
-								Evacuation Essentials
+								Assembling your Wildfire-Ready Kit
 							</h2>
 							<p className="text-sm text-gray-700">
-								Understand the true burden imposed by loss—in money, lost time, poorer health,
-								damaged productivity and lowered job security. This is some more information to test how much text can fit in the box.
+								Be proactive with a well-prepared emergency supply kit.
 							</p>
 						</a>
 					</div>
 				</div>
-				<div className="max-w-sm bg-white rounded-2xl shadow-md">
+				<div className="max-w-xs bg-white text-left rounded-2xl shadow-md">
 					<img 
-						src="/images/resource1.png" 
+						src="/images/resource1b.png" 
+						alt="Evacuation Essentials"
+						className="w-full object-cover rounded-t-2xl"
+					/>
+					<div className="p-4">
+						<p className="text-sm text-gray-600 uppercase tracking-wide mb-2">American Red Cross</p>
+						<a href="https://www.redcross.org/get-help/how-to-prepare-for-emergencies/types-of-emergencies/wildfire.html?srsltid=AfmBOopRTJ8Nh58i4OClG0bJSb908NNxzmBSkWNO0695k2hdOEJruFdf">
+							<h2 className="text-lg font-semibold text-gray-800 mb-2">
+								What Should You Do Before a Wildfire Starts?
+							</h2>
+							<p className="text-sm text-gray-700">
+								More people are living in areas at risk for wildfires, but we can take action to prepare.
+							</p>
+						</a>
+					</div>
+				</div>
+				<div className="max-w-xs bg-white text-left rounded-2xl shadow-md">
+					<img 
+						src="/images/resource1c.png" 
+						alt="Evacuation Essentials"
+						className="w-full object-cover rounded-t-2xl"
+					/>
+					<div className="p-4">
+						<p className="text-sm text-gray-600 uppercase tracking-wide mb-2">National Weather Service</p>
+						<a href="https://www.weather.gov/safety/wildfire-ready">
+							<h2 className="text-lg font-semibold text-gray-800 mb-2">
+								Home Preparedness
+							</h2>
+							<p className="text-sm text-gray-700">
+								Take personal responsibility and prepare long before the threat of a wildland 
+								fire so your home is ready in case of a fire
+							</p>
+						</a>
+					</div>
+				</div>
+			</div>
+			<h2 className="text-left text-2xl mt-8">Wildire Survival</h2>
+			<div className="flex flex-wrap gap-x-10 gap-y-10"> 
+				{/* Resource Card */}
+				<div className="max-w-xs text-left bg-white rounded-2xl shadow-md">
+					<img 
+						src="/images/resource2a.png" 
+						alt="Evacuation Essentials"
+						className="w-full object-cover rounded-t-2xl"
+					/>
+					<div className="p-4">
+						<p className="text-sm text-gray-600 uppercase tracking-wide mb-2">California State Parks</p>
+						<a href="https://www.parks.ca.gov/?page_id=30661">
+							<h2 className="text-lg font-semibold text-gray-800 mb-2">
+								Wildfire Safety Tips
+							</h2>
+							<p className="text-sm text-gray-700">
+							Whether you are an experienced outdoor enthusiast or a new one, California State Parks looks forward to seeing you at 
+							California’s park trails, beaches, mountains, and deserts but ask that you recreate responsibly.
+							</p>
+						</a>
+					</div>
+				</div>
+				<div className="max-w-xs bg-white text-left rounded-2xl shadow-md">
+					<img 
+						src="/images/resource2b.png" 
+						alt="Evacuation Essentials"
+						className="w-full object-cover rounded-t-2xl"
+					/>
+					<div className="p-4">
+						<p className="text-sm text-gray-600 uppercase tracking-wide mb-2">Ruidoso New Mexico Gov</p>
+						<a href="https://www.ruidoso-nm.gov/wildfire-safety-tips">
+							<h2 className="text-lg font-semibold text-gray-800 mb-2">
+								Everything you need to know to protect you, your family and property during a wildfire 
+							</h2>
+							<p className="text-sm text-gray-700">
+							Knowing what to do in the event of a wildfire can help save your life and property.  
+							Learn how to protect you, your family and property during a rural fire with these essential wildfire safety tips.
+							</p>
+						</a>
+					</div>
+				</div>
+				<div className="max-w-xs bg-white text-left rounded-2xl shadow-md">
+					<img 
+						src="/images/resource2c.png" 
+						alt="Evacuation Essentials"
+						className="w-full object-cover rounded-t-2xl"
+					/>
+					<div className="p-4">
+						<p className="text-sm text-gray-600 uppercase tracking-wide mb-2">Ready.GOV</p>
+						<a href="https://readyforwildfire.org/prepare-for-wildfire/emergency-supply-kit/">
+							<h2 className="text-lg font-semibold text-gray-800 mb-2">
+								Wildfires
+							</h2>
+							<p className="text-sm text-gray-700">
+							Wildfires are unplanned fires that burn in natural areas like forests, grasslands or prairies. 
+							These dangerous fires spread quickly and can devastate not only wildlife and natural areas, but also communities.
+							</p>
+						</a>
+					</div>
+				</div>
+				<div className="max-w-xs bg-white text-left rounded-2xl shadow-md">
+					<img 
+						src="/images/resource2d.webp" 
+						alt="Evacuation Essentials"
+						className="w-full object-cover rounded-t-2xl"
+					/>
+					<div className="p-4">
+						<p className="text-sm text-gray-600 uppercase tracking-wide mb-2">Los Angeles Fire Department</p>
+						<a href="https://lafd.org/ready-set-go#:~:text=Create%20a%20Wildfire%20Action%20Plan,important%20evacuation%20and%20contact%20information.">
+							<h2 className="text-lg font-semibold text-gray-800 mb-2">
+								Ready, Set, Go!
+							</h2>
+							<p className="text-sm text-gray-700">
+								If there is an active wildfire in your area, you need to be prepared before it’s time to Go!
+							</p>
+						</a>
+					</div>
+				</div>
+			</div>
+			<h2 className="text-left text-2xl mt-8">Wildfire Response</h2>
+			<div className="flex flex-wrap gap-x-10 gap-y-10 "> 
+				{/* Resource Card */}
+				<div className="max-w-xs text-left bg-white rounded-2xl shadow-md">
+					<img 
+						src="/images/resource3a.jpg" 
 						alt="Evacuation Essentials"
 						className="w-full object-cover rounded-t-2xl"
 					/>
 					<div className="p-4">
 						<p className="text-sm text-gray-600 uppercase tracking-wide mb-2">Ready for Wildfire</p>
-						<a href="https://readyforwildfire.org/prepare-for-wildfire/emergency-supply-kit/">
+						<a href="https://readyforwildfire.org/post-wildfire/after-a-wildfire/">
 							<h2 className="text-lg font-semibold text-gray-800 mb-2">
-								Evacuation Essentials
+								After a Wildfire
 							</h2>
 							<p className="text-sm text-gray-700">
-								Understand the true burden imposed by loss—in money, lost time, poorer health,
-								damaged productivity and lowered job security. This is some more information to test how much text can fit in the box.
+								From damage assessment to future planning, get all the advice you need to recover from a wildfire.
+							</p>
+						</a>
+					</div>
+				</div>
+				<div className="max-w-xs bg-white text-left rounded-2xl shadow-md">
+					<img 
+						src="/images/resource3b.jpg" 
+						alt="Evacuation Essentials"
+						className="w-full object-cover rounded-t-2xl"
+					/>
+					<div className="p-4">
+						<p className="text-sm text-gray-600 uppercase tracking-wide mb-2">Ready for Wildfire</p>
+						<a href="https://readyforwildfire.org/post-wildfire/returning-home/">
+							<h2 className="text-lg font-semibold text-gray-800 mb-2">
+								Returning Home Checklist
+							</h2>
+							<p className="text-sm text-gray-700">
+								The danger doesn’t end when the wildfire is extinguished. The aftermath can present various hazards, 
+								from flash flooding and debris flows to structural instability and compromised trees.
 							</p>
 						</a>
 					</div>
 				</div>
 			</div>
 		</div>
+		
 	  );
 }
